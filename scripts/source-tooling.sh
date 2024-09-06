85212b8a3904c9d899824564bbb978c870c84d0c
Bram Goolaerts <dev@bollewolle.be>
2024-09-06 17:57:42 +0200

    update tooling script

--- scripts/source-tooling.sh
+++ scripts/source-tooling.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Source tooling
 if [ -f "/usr/local/bin/brew" ]; then
   export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
@@ -11,10 +12,10 @@ if [ -f "/opt/linuxbrew/bin/brew" ]; then
   export PATH="/opt/linuxbrew/bin:/opt/linuxbrew/sbin:$PATH"
 fi
 
-if command -v brew &> /dev/null; then
+if command -v brew > /dev/null; then
   eval "$(brew shellenv)"
 fi
 
 if [ -f "$HOME/.asdf/asdf.sh" ]; then
-  source "$HOME/.asdf/asdf.sh"
+  . "$HOME/.asdf/asdf.sh"
 fi
