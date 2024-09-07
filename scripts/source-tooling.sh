096f61406391a8b038b281d68bcb8f4a07f33d97
Bram Goolaerts <dev@bollewolle.be>
2024-09-07 09:56:20 +0200

    Revert "update tooling script"
    This reverts commit 85212b8a3904c9d899824564bbb978c870c84d0c.

--- scripts/source-tooling.sh
+++ scripts/source-tooling.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 # Source tooling
 if [ -f "/usr/local/bin/brew" ]; then
   export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
@@ -12,10 +11,10 @@ if [ -f "/opt/linuxbrew/bin/brew" ]; then
   export PATH="/opt/linuxbrew/bin:/opt/linuxbrew/sbin:$PATH"
 fi
 
-if command -v brew > /dev/null; then
+if command -v brew &> /dev/null; then
   eval "$(brew shellenv)"
 fi
 
 if [ -f "$HOME/.asdf/asdf.sh" ]; then
-  . "$HOME/.asdf/asdf.sh"
+  source "$HOME/.asdf/asdf.sh"
 fi
